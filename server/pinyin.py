8897cca6da35f3cb10dfdae42e4adceedab7d98b
Shuo Tian <tianshuo1996@outlook.com>
2022-02-15 20:34:31 -0600

    backend add pinyin tone

--- server/pinyin.py
+++ server/pinyin.py
@@ -1,5 +1,13 @@
+from dataclasses import dataclass
 from pypinyin import lazy_pinyin, load_single_dict
 import pandas as pd
+
+
+@dataclass
+class CharDetails:
+    char: str
+    pinyin_tone: str
+
 
 load_single_dict({ord("什"): "shí,shén"})
 
@@ -15,13 +23,13 @@ def get_pinyin(char: str) -> str:
     return pinyin
 
 
-def beautify_ming(original: str, gender: str) -> list[list[str]]:
+def beautify_ming(original: str, gender: str) -> list[list[CharDetails]]:
     if gender.lower() not in ["m", "f"]:
         raise ValueError("Gender must be either M or F.")
 
     two_char_ming = original[:2]
 
-    ming_chars_options: list[list[str]] = []
+    char_details_options: list[list[CharDetails]] = []
 
     for i, char in enumerate(two_char_ming):
         position = i + 1
@@ -32,17 +40,20 @@ def beautify_ming(original: str, gender: str) -> list[list[str]]:
             & (df_ming["position"] == position)
             & (df_ming["gender"] == gender.lower())
         ]
-        options: list[str] = df_heteronym["char"].to_list()
-        if len(options) != 0:
-            heteronym = options[0:5]
+        char_details_list: list[CharDetails] = [
+            {"char": row[1]["char"], "pinyin_tone": row[1]["pinyin_tone"]}
+            for row in df_heteronym.iterrows()
+        ]
+        if len(char_details_list) != 0:
+            chosen_char_details = char_details_list[0:5]
         else:
-            heteronym = [char]
-        ming_chars_options.append(heteronym)
+            chosen_char_details = [char]
+        char_details_options.append(chosen_char_details)
 
-    return ming_chars_options
+    return char_details_options
 
 
-def beautify_xing(original: str, position=0) -> list[str]:
+def beautify_xing(original: str, position=0) -> list[CharDetails]:
     length = len(original)
     if length == 0:
         raise ValueError("I don't know how to pronounce this name :/")
@@ -52,7 +63,10 @@ def beautify_xing(original: str, position=0) -> list[str]:
     one_char_xing = original[position]
     pinyin = get_pinyin(one_char_xing)
 
-    all_options: list[str] = df_xing[df_xing["pinyin"] == pinyin]["xing"].to_list()
+    all_options: list[CharDetails] = [
+        {"char": row[1]["xing"], "pinyin_tone": row[1]["pinyin_tone"]}
+        for row in df_xing[df_xing["pinyin"] == pinyin].iterrows()
+    ]
 
     if len(all_options) == 0:
         if position == length - 1:
