64e173968644a13cb5d78ee1d6f07da0f3427060
Shuo Tian <tianshuo1996@outlook.com>
2022-02-16 16:59:03 -0600

    add pronunciation to the API

--- server/pinyin.py
+++ server/pinyin.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from pypinyin import lazy_pinyin, load_single_dict, Style
+from pypinyin.contrib.tone_convert import to_tone3
 import pandas as pd
 
 
@@ -7,6 +8,7 @@ import pandas as pd
 class CharDetails:
     char: str
     pinyin: str
+    pronunciation: str
 
 
 load_single_dict({ord("什"): "shí,shén"})
@@ -20,8 +22,12 @@ xing_pinyins: list[str] = list(df_xing["pinyin"].unique())
 
 def get_pinyin(char: str, style: Style = Style.NORMAL) -> str:
     pinyin = lazy_pinyin(char, style)[0]
-
     return pinyin
+
+
+def get_pronunciation(pinyin: str) -> str:
+    pinyin_tone3 = to_tone3(pinyin)
+    return f"https://github.com/shikangkai/Chinese-Pinyin-Audio/blob/master/Pinyin-Female/{pinyin_tone3}.mp3?raw=true"
 
 
 def beautify_ming(original: str, gender: str) -> list[list[CharDetails]]:
@@ -42,7 +48,11 @@ def beautify_ming(original: str, gender: str) -> list[list[CharDetails]]:
             & (df_ming["gender"] == gender.lower())
         ]
         char_details_list: list[CharDetails] = [
-            {"char": row[1]["char"], "pinyin": row[1]["pinyin_tone"]}
+            {
+                "char": row[1]["char"],
+                "pinyin": row[1]["pinyin_tone"],
+                "pronunciation": get_pronunciation(row[1]["pinyin_tone"]),
+            }
             for row in df_heteronym.iterrows()
         ]
         if len(char_details_list) != 0:
@@ -65,14 +75,25 @@ def beautify_xing(original: str, position=0) -> list[CharDetails]:
     pinyin = get_pinyin(one_char_xing)
 
     all_options: list[CharDetails] = [
-        {"char": row[1]["xing"], "pinyin": row[1]["pinyin_tone"]}
+        {
+            "char": row[1]["xing"],
+            "pinyin": row[1]["pinyin_tone"],
+            "pronunciation": get_pronunciation(row[1]["pinyin_tone"]),
+        }
         for row in df_xing[df_xing["pinyin"] == pinyin].iterrows()
     ]
 
     if len(all_options) == 0:
         if position == length - 1:
             char = original[0]
-            return [{"char": char, "pinyin": get_pinyin(char, Style.TONE)}]
+            char_pinyin = get_pinyin(char, Style.TONE)
+            return [
+                {
+                    "char": char,
+                    "pinyin": char_pinyin,
+                    "pronunciation": get_pronunciation(char_pinyin),
+                }
+            ]
         return beautify_xing(original, position + 1)
     return all_options
 
