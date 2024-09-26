0f4746de01f580acee8985f336391ae8bf4b01e2
Shuo Tian <tianshuo1996@outlook.com>
2024-09-25 22:13:18 -0500

    update AI

--- utils/translate.ts
+++ utils/translate.ts
@@ -1,45 +1,13 @@
-import axios from "axios";
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
 
-const TRANSLATE_API_URL = "https://www.googleapis.com/language/translate/v2";
-const TRANSLATION_KEY = process.env["TRANSLATION_KEY"];
-
-const META_PARAMS = {
-  key: TRANSLATION_KEY,
-  target: "zh-CN",
-  mimeType: "text/plain",
-};
-
-const QUERY_PREFIX = "my name is ";
-const RESULT_PREFIXES = ["我的名字是", "我的名字叫", "我的名字", "我叫"];
-
-const translate = async (
-  originalName: string,
-  shouldTryAgain = true
-): Promise<string> => {
-  const params = {
-    q: QUERY_PREFIX + originalName,
-    ...META_PARAMS,
-  };
-
-  const response = await axios.get(TRANSLATE_API_URL, { params });
-  let translation: string =
-    response.data["data"]["translations"][0]["translatedText"];
-
-  for (const prefix of RESULT_PREFIXES) {
-    if (translation.includes(prefix)) {
-      translation = translation.split(prefix)[1];
-    }
-  }
-  const CHINSESE_REGEX =
-    /[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]+/;
-  console.log({ translation });
-  if (!CHINSESE_REGEX.test(translation)) {
-    if (!shouldTryAgain)
-      throw new Error("We don't know how to pronounce this name");
-    // Try it one more time, but that's it
-    return translate(originalName.toLowerCase(), false);
-  }
-  return translation;
+const translate = async (input: string) => {
+  const { text } = await generateText({
+    model: openai("gpt-4o"),
+    system: "Translate this name into Chinese. do not say anything else.",
+    prompt: input,
+  });
+  return text;
 };
 
 export default translate;
